f43935c8f8a0ccf532c64a6b390852318fe10067
DavidB <david.burns.dtb@googlemail.com>
2019-04-03 21:03:26 +0100

    Updates

---
+++ precache-manifest.c1fd2292d3f8ea8679247dd0d4085ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "870bc9f4045950c53165",
+    "url": "/react-gh-pages/static/js/main.53ce2484.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-gh-pages/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "870bc9f4045950c53165",
+    "url": "/react-gh-pages/static/css/main.a19253bf.chunk.css"
+  },
+  {
+    "revision": "bff4582ece3b199dc7fe6c8b0b7b50af",
+    "url": "/react-gh-pages/index.html"
+  }
+];
